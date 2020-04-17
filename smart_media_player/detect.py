0f7187f1f0bb6d0580788e9e7303e80288a07458
bdjbray <49975301+bdjbray@users.noreply.github.com>
2020-04-17 12:14:05 -0400

    Add files via upload

--- smart_media_player/detect.py
+++ smart_media_player/detect.py
@@ -1,8 +1,10 @@
 import cv2
 import cvlib as cv
+import os
 
 def detect():
-	img = cv2.imread("/Users/brayb/Downloads/pythonws/ec500/homework5_6/smart_media_player/pics/frame5.jpg")
+	path=os.getcwd()
+	img = cv2.imread(path+"/pics/frame5.jpg")
 	boxes, labels, _conf = cv.detect_common_objects(img, model="yolov3")
 
 	print(labels)
